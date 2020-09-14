adcc6abf53b923b34441dce83a8ac927ecfad8f8
farida2 <fari_belh@yahoo.co.uk>
2020-09-14 19:59:25 +0000

    Calculator second commit

--- calculator/src/calculator/Calculator.java
+++ calculator/src/calculator/Calculator.java
@@ -14,7 +14,7 @@ public class Calculator {
 	}
 	
 	//constructor with parameters double x and double y
-	public Calculator(double x, double y) { // good practice
+	public Calculator(double x, double y) { 
 	    	this.x = x;
 	    	this.y = y;
 			
@@ -25,46 +25,12 @@ public class Calculator {
 	public double add() {
 		return x + y;
 	}
-//	public void add(int a, int b) {
-//		System.out.println("void(int,int) was calles and result is " + (a + b));
-//	}
-//	
-//	public int add(int a, int b, int c) {
-//		System.out.println("void(int,int) was calles and result is ");
-//		return a + b + c;
-//	}
-	
-//	public void add(float a, float b) {
-//		System.out.println("void(int,int) was calles and result is " + (a + b));
-//	}
-//	
-	
-//	// subtraction between two values of type int
-//	public int subtract(int x, int y) {
-//		return x - y;
-//	}
-//	
-//	// subtraction between two values of type float
-//	public float subtract(float x, float y) {
-//		return x - y;
-//	}
-	
+
 	// subtraction between two values of type double
 	public double subtract(double x, double y) {
 		return x - y;
 	}
 	
-//	// subtraction between two values of type long 
-//	public long subtract(long x, long y) {
-//			return x - y;
-//		}
-//	
-	  
-//	// multiplication between two values of type int
-//	public int multiply(int x, int y) {
-//			return x * y;	
-//		}
-//	
 	
 	// multiplication between two values of type double
 		public double multiply(double x, double y) {
